f249c39bfd4e848de5b19963a9422d7b1b6d92f6
abdulmejidamk <abdulmejidamk@gmail.com>
2024-01-17 02:34:09 +0300

    Env Template Render

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const path = require("path");
 const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: path.join("./config.env") });
+
 const app = express();
 
 // Start Database
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "netcrawler",
-  password: "netcrawler",
-  database: "nodejs_login",
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
 });
+
+// Telling Node when sattic files are
+const publicDirection = path.join(__dirname, "./public");
+app.use(express.static(publicDirection));
+// Template Engine
+app.set("view engine", "hbs");
 
 // Connect Database
 db.connect((error) => {
@@ -20,7 +31,8 @@ db.connect((error) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("<h1>Home Page</h1>");
+  //   res.send("<h1>Home Page</h1>");
+  res.render("index");
 });
 
 app.listen(5000, (req, res) => {
