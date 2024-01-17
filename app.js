c73cc2e377a23066c22272ec3ae4316df2e31f78
abdulmejidamk <abdulmejidamk@gmail.com>
2024-01-17 10:43:32 +0300

    "| auth controllers |"

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ const db = mysql.createConnection({
   database: "nodejs_login",
 });
 
+<<<<<<< Updated upstream
+=======
+// Telling Node when sattic files are
+const publicDirection = path.join(__dirname, "./public");
+app.use(express.static(publicDirection));
+app.use(express.json());
+
+// Template Engine
+app.set("view engine", "hbs");
+
+>>>>>>> Stashed changes
 // Connect Database
 db.connect((error) => {
   if (error) {
@@ -19,9 +30,15 @@ db.connect((error) => {
   }
 });
 
+<<<<<<< Updated upstream
 app.get("/", (req, res) => {
   res.send("<h1>Home Page</h1>");
 });
+=======
+// Defining routes
+app.use("/", require("./routes/pages"));
+app.use("/auth", require("./routes/auth"));
+>>>>>>> Stashed changes
 
 app.listen(5000, (req, res) => {
   console.log("Server listening on port 5000 ......");
